3ecd577473217941a9d6a1267240dca5ec677a86
wangxianyu7 <xianyuwang7@gmail.com>
2025-05-05 20:49:07 -0400

    Update README

--- catalog.py
+++ catalog.py
@@ -32,11 +32,11 @@ image_base64 = get_base64_image("logo.png")
 st.markdown(f"""
     <h2>
         <img src="{image_base64}" alt="SOcat Logo" style="height: 60px; vertical-align: middle; margin-right: 10px;">
-        <span style="color: black;">SO</span><span style="color: red;">Cat</span>: Stellar Obliquity Measurements from Global Modeling to Rossiter-McLaughlin Effects
+        <span style="color: black;">SO</span><span style="color: red;">Cat</span>: Stellar Obliquity Measurements for Exoplanets and Brown Dwarfs
     </h2>
     <details>
         <summary>About</summary>
-        <p>RMcat is a catalog of stellar obliquity measurements from global modeling to Rossiter-McLaughlin effects. It includes data from various sources and provides a comprehensive overview of the current state of research in this field.</p>
+        <p>SOcat is a catalog of stellar obliquity measurements for exoplanets and brown dwarfs. It includes data from various sources and provides a comprehensive overview of the current state of research in this field.</p>
     </details>
 """, unsafe_allow_html=True)
 
