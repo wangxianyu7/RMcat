4f2df36bccd8e546c4d6afcbfc830eabf31bf842
xianyuwang7 <xianyuwnag7@gmail.com>
2025-05-08 23:31:14 -0400

    some typos fixed

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 # Define the pages
 main_page = st.Page("catalog.py", title="Catalog", icon="")
 page_2 = st.Page("simulator.py", title="RM simulator", icon="")
-page_21 = st.Page("chatbot.py", title="Obliquity Chatbot", icon="")
+page_21 = st.Page("chatbot.py", title="Obliquity chatbot", icon="")
 page_3 = st.Page("softwares.py", title="Modeling softwares", icon="")
-page_4 = st.Page("psicalculator.py", title="True Obliquity (ψ) calculator", icon="")
+page_4 = st.Page("psicalculator.py", title="True obliquity calculator", icon="")
 page_5 = st.Page("miscellaneous.py", title="Miscellaneous", icon="")
 
 # Set up navigation
